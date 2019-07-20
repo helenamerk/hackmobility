ca8e9b9c725bb8ca3aa172fac009467b739b4946
xo28122000 <jainam.2000@yahoo.com>
2019-07-20 16:18:20 -0700

    initialized expo

--- hackmobility/src/components/Analytics.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-class Analytics extends Component {
-  state = {};
-  render() {
-    return <div>Analytics</div>;
-  }
-}
-
-export default Analytics;
--- hackmobility/src/components/MapArea.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-class MapArea extends Component {
-  state = {};
-  render() {
-    return <div>MapArea</div>;
-  }
-}
-
-export default MapArea;
